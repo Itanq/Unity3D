6659eb0537a4e437793a800f957b56f3276c625c
zmant <1187795440@qq.com>
2016-09-02 09:59:06 +0800

    test

---
+++ test/Assets/scripts/move.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+	}
+
+    void Move()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            Rigidbody rb = GetComponent<Rigidbody>();
+        }
+    }
+}
