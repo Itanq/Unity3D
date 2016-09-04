fb2aed8b4a27995989c7d46e0e35c3f31010ddf2
zmant <1187795440@qq.com>
2016-09-04 20:27:22 +0800

    ParticleCallback first commit

---
+++ ParticleCallback/Assets/Scripts/ParticleHelper.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class ParticleHelper{
+
+    public ParticleSystem particle;
+    public Light light;
+
+    public bool varyAlpha;
+    public float minAlpha, maxAlpha;
+    public float alphaIncreaseRate, alphaDecreaseRate;
+    public float alphaVariation;
+
+    public void IncreaseAlpha()
+    {
+        if(particle.startColor.a < maxAlpha)
+        {
+            Color adjustedColor = particle.startColor;
+            adjustedColor.a += alphaIncreaseRate * Time.deltaTime;
+            adjustedColor.a += Random.Range(0.0f, alphaVariation);
+            particle.startColor = adjustedColor;
+        }
+    }
+
+    public void DecreaseAlpha()
+    {
+        if(particle.startColor.a>minAlpha)
+        {
+            Color adjustedColor = particle.startColor;
+            adjustedColor.a -= alphaDecreaseRate * Time.deltaTime;
+            particle.startColor = adjustedColor;
+        }
+    }
+
+    public bool varyLight;
+    public float minLight, maxLight;
+    public float lightIncreaseRate, lightDecreaseRate;
+    public float lightVariation;
+
+    public void IncreaseLight()
+    {
+        if(light.intensity < maxLight)
+            light.intensity += lightIncreaseRate * Time.deltaTime;
+    }
+
+    public void DecreaseLight()
+    {
+        if(light.intensity > minLight)
+            light.intensity -= lightDecreaseRate * Time.deltaTime;
+    }
+}
